23f04a24c62c0b45c0195ef247bab05e95e5e1c0
Mike <mirtrudmike@gmail.com>
2024-06-02 04:26:46 +0400

    fixed time for breakfast ask

--- functions/chelduler_functions.py
+++ functions/chelduler_functions.py
@@ -77,8 +77,8 @@ def set_schedulers(bot: Bot, user_ids: list, scheduler: AsyncIOScheduler, bnovo_
                       )
     scheduler.add_job(ask_breakfast,
                       trigger='cron',
-                      hour="4",
-                      minute="19",
+                      hour="15",
+                      minute="55",
                       kwargs={'bot': bot,
                               'user_ids': user_ids,
                               'bnovo_login': bnovo_login,
